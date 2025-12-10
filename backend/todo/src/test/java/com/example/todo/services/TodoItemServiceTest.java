603501e8755c29b756a102ca8f06624422a09cc4
PAKO25 <58482136+PAKO25@users.noreply.github.com>
2025-12-10 14:49:38 +0100

    5 testov, porocilo, popravljen sql_setup

---
+++ backend/todo/src/test/java/com/example/todo/services/TodoItemServiceTest.java
@@ -0,0 +1,16 @@
+package com.example.todo.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TodoItemServiceTest {
+
+    @Test
+    void updateTodo() {
+    }
+
+    @Test
+    void toggleTodoCompletion() {
+    }
+}
